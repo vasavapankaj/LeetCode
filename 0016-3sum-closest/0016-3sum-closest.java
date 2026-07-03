dc87961b2c4e1bd024a6dd89482a7a94109d19b4
Vasava Pankajkumar Kanubhai <70348309+vasavapankaj@users.noreply.github.com>
2026-07-03 21:53:53 +0530

    Time: 18 ms (41.1%), Space: 45.6 MB (48.09%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -8,9 +8,6 @@ class Solution {
         Arrays.sort(nums);
 
         for(i=0;i<n-2;i++){
-            if (i > 0 && nums[i] == nums[i-1]) {
-                continue;
-            }
             left = i+1;
             right = n-1;
             while(left<right){
